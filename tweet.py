1c0ffcae534da9718f3f1a8ea8abb021b51b69e3
2spi <314twen7@gmail.com>
2023-06-20 00:04:11 +0530

    Updated tweet.py

--- tweet.py
+++ tweet.py
@@ -11,16 +11,12 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-CONSUMER_KEY = environ["CONSUMER_KEY"]
-CONSUMER_SECRET = environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = environ["ACCESS_TOKEN"]
-ACCESS_SECRET = environ["ACCESS_SECRET"]
-
-auth = tweepy.OAuth1UserHandler(
-    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
+client = tweepy.Client(
+    consumer_key=environ["CONSUMER_KEY"],
+    consumer_secret=environ["CONSUMER_SECRET"],
+    access_token=environ["ACCESS_TOKEN"],
+    access_token_secret=environ["ACCESS_SECRET"]
 )
-
-api = tweepy.API(auth)
 
 gc = gspread.service_account(filename='gsheet-credentials.json')
 sh = gc.open_by_key('1nQ1EjWsjd4Cr_Ixlyijvu0-fXDcjbcbaj9DLDoyC__k')
@@ -33,7 +29,7 @@ def main():
     while True:
         tweet_records = worksheet.get_all_records()
         cur_time = datetime.utcnow() + timedelta(hours=5, minutes=30)
-        logger.info(f'{len(tweet_records)} tweets scheduled at {cur_time.time()}')
+        logger.info(f'{len(tweet_records)} tweets found at {cur_time.time()}')
         time.sleep(INTERVAL)
 
         for idx, tweet in enumerate(tweet_records, start=2):
@@ -46,7 +42,7 @@ def main():
                 c_time = datetime.utcnow() + timedelta(hours=5, minutes=30)
                 if date_time_obj < c_time:
                     try:
-                        # api.update_status(content)
+                        client.create_tweet(text=content)
                         worksheet.update_cell(idx, 3, 1)
                     except Exception as e:
                         logger.warning(f'Exception during tweet! {e}')
